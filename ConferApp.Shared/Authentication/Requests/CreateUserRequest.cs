ca919de454efec09a19534465b0cb4e4f2b4b05d
bokoviaroslav <iaroslavbokov@yandex.ru>
2025-03-20 22:28:37 +0300

    deleted fluentui; aded registration component

--- ConferApp.Shared/Authentication/Requests/CreateUserRequest.cs
+++ ConferApp.Shared/Authentication/Requests/CreateUserRequest.cs
@@ -1,10 +1,19 @@
-﻿namespace ConferApp.Shared.Authentication.Requests
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConferApp.Shared.Authentication.Requests
 {
     public class CreateUserRequest
     {
+        [MaxLength(30)]
         public string Name { get; set; } = string.Empty;
+
+        [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
+
+        [Required, MaxLength(50)]
         public string Password { get; set; } = string.Empty;
+
+        [Required, Compare(nameof(Password))]
         public string PasswordConfirmation {  get; set; } = string.Empty;
     }
 }
--- ConferApp.Shared/Authentication/Requests/LoginUserRequest.cs
+++ ConferApp.Shared/Authentication/Requests/LoginUserRequest.cs
@@ -1,8 +1,13 @@
-﻿namespace ConferApp.Shared.Authentication.Requests
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ConferApp.Shared.Authentication.Requests
 {
     public class LoginUserRequest
     {
+        [Required, EmailAddress]
         public string Email { get; set; } = string.Empty;
+
+        [Required, MaxLength(50)]
         public string Password { get; set; } = string.Empty;
     }
 }
