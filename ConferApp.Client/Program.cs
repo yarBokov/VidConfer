3556c4c11e7edf0de4e08a993dd0ab71b9abf3c1
bokoviaroslav <iaroslavbokov@yandex.ru>
2025-03-22 21:44:42 +0300

    dashboard

--- ConferApp.Client/Program.cs
+++ ConferApp.Client/Program.cs
@@ -1,4 +1,5 @@
 using ConferApp.Client;
+using ConferApp.Client.States;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Radzen;
@@ -8,6 +9,9 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+//states
+builder.Services.AddScoped<NavigationState>();
 
 //radzen
 builder.Services.AddRadzenComponents();
